bf56861aaa3d400f3573b39fc51c05f389cf2653
vinaykumarkolepaka <vinaykumarkolepaka@gmail.com>
2024-04-21 00:21:39 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 (function ($) {
     "use strict";
-
+    $(document).ready(function () {
     window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-3BDVCJGXP1');
+    });
 
     // Navbar on scrolling
     $(window).scroll(function () {
